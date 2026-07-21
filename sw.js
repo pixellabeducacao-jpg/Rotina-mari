df5fa56240f0a377b4cc24449d2cff0af9f06079
pixellabeducacao-jpg <pixellabeducacao@gmail.com>
2026-07-21 08:18:33 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'ritmo-v1';
+const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('index.html')))
+  );
+});
